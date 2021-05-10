0a048ecbfa644cadb65331a75fbceb537a25c46e
Andrey <sereginaa20@yandex.ru>
2021-05-10 15:40:53 +0400

    7

--- eShop_back/server.js
+++ eShop_back/server.js
@@ -47,14 +47,9 @@ app.post('/addToCart', (req, res) => {
         } else {
             const cart = JSON.parse(data);
             const item = req.body;
-            // const hasItem = cart.find((good) => good.product_name === item.product_name);
-
-            // if (hasItem) {
-            //     res.send('{"result": 1}');
-            //     return;
-            // }
 
             cart.push(item);
+
             fs.writeFile('./database/cart.json', JSON.stringify(cart), (err) => {
                 if (err) {
                     res.send('{"result": 0}');
